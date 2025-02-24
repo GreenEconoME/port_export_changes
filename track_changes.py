68a8d2c0284e8c74a77df6cc868b804e58504433
unknown <kyle@greeneconome.com>
2025-02-24 14:01:45 -0800

    Add credential requirement for access

--- track_changes.py
+++ track_changes.py
@@ -1,4 +1,6 @@
 # Import dependencies
+import yaml
+import traceback
 import streamlit as st
 import pandas as pd
 
@@ -17,6 +19,7 @@ repo_owner = st.secrets['REPO_OWNER']
 repo_name = st.secrets['REPO_NAME']
 branch = st.secrets['BRANCH']
 image_path = st.secrets['IMAGE_PATH']
+credential_key = st.secrets['CREDENTIAL_KEY']
 
 
 # Download and display the logo
@@ -27,68 +30,89 @@ if logo_image:
 # Set a title for the page
 st.markdown("<h2 style = 'text-align: center; color: black;'>Dashboard Portfolio Export</br>What's Changed?</h2>", unsafe_allow_html = True)
 
-st.subheader("Upload Old Report")
-old_upload = st.file_uploader("Upload the older portfolio export.", type="xlsx")
 
-st.subheader("Upload New Report")
-new_upload = st.file_uploader("Upload the new portfolio export.", type="xlsx")
+# Handle credential file upload
+st.subheader("Upload Credentials")
+cred_upload = st.file_uploader("Upload credentials file (crecential_key.yaml)", type = "yaml")
+if cred_upload is not None:
+    try:
+        credentials = yaml.safe_load(cred_upload)
 
-# Ensure both reports have been uploaded
-if old_upload is not None and new_upload is not None:
-    if st.button('Click to start processing differences'):
-        # Initialize progress tracking with file loading
-        st.session_state["sheet_status"] = {
-            "Loading Old Report": "Pending",
-            "Loading New Report": "Pending",
-            **{sheet: "Pending" for sheet in sheets_to_compare},
-            "Generating Download": "Pending"
-        }
+        # Get credential key from credential file
+        yaml_credential = credentials.get('credential_key')
+        if yaml_credential == credential_key:
 
-        # Create a placeholder for progress updates
-        progress_placeholder = st.empty()
-        progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
+            st.subheader("Upload Old Report")
+            old_upload = st.file_uploader("Upload the older portfolio export.", type="xlsx")
 
-        # Read in the workbooks
-        # Start loading OLD report
-        st.session_state["sheet_status"]["Loading Old Report"] = "Processing"
-        progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
-        with pd.ExcelFile(old_upload) as f:
-            old = pd.read_excel(f, sheet_name = None)
+            st.subheader("Upload New Report")
+            new_upload = st.file_uploader("Upload the new portfolio export.", type="xlsx")
 
-        # Set reading in old report as completed
-        st.session_state["sheet_status"]["Loading Old Report"] = "Completed"
-        progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
 
-        # Start loading NEW report
-        st.session_state["sheet_status"]["Loading New Report"] = "Processing"
-        progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
 
-        with pd.ExcelFile(new_upload) as f:
-            new = pd.read_excel(f, sheet_name = None)
+            # Ensure both reports have been uploaded
+            if old_upload is not None and new_upload is not None:
+                if st.button('Click to start processing differences'):
+                    # Initialize progress tracking with file loading
+                    st.session_state["sheet_status"] = {
+                        "Loading Old Report": "Pending",
+                        "Loading New Report": "Pending",
+                        **{sheet: "Pending" for sheet in sheets_to_compare},
+                        "Generating Download": "Pending"
+                    }
 
-        # Set reading in the new report as completed
-        st.session_state["sheet_status"]["Loading New Report"] = "Completed"
-        progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
+                    # Create a placeholder for progress updates
+                    progress_placeholder = st.empty()
+                    progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
 
-        # Remove column not needing comparison
-        old['Meter Activity'].drop(columns = [x for x in old['Meter Activity'].columns if 'Days Since Upload as of' in x], inplace = True)
-        new['Meter Activity'].drop(columns = [x for x in new['Meter Activity'].columns if 'Days Since Upload as of' in x], inplace = True)
+                    # Read in the workbooks
+                    # Start loading OLD report
+                    st.session_state["sheet_status"]["Loading Old Report"] = "Processing"
+                    progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
+                    with pd.ExcelFile(old_upload) as f:
+                        old = pd.read_excel(f, sheet_name = None)
 
-        # Generate the what's changed report
-        result = gen_report(unique_keys, sheets_to_compare, progress_placeholder, old, new)
+                    # Set reading in old report as completed
+                    st.session_state["sheet_status"]["Loading Old Report"] = "Completed"
+                    progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
 
-        # Store result in session_state to prevent rerunning upon download
-        st.session_state['changed_report'] = result
+                    # Start loading NEW report
+                    st.session_state["sheet_status"]["Loading New Report"] = "Processing"
+                    progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
 
-        # Offer the file for download
-        if 'changed_report' in st.session_state:
-            st.download_button(
-                label = "Download What's Changed Report",
-                data = st.session_state["changed_report"],
-                file_name = f"{datetime.now().strftime('%Y-%m-%d')} Changes.xlsx",
-                mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
-            )
+                    with pd.ExcelFile(new_upload) as f:
+                        new = pd.read_excel(f, sheet_name = None)
 
-            # set generating export to completed
-            st.session_state["sheet_status"]["Generating Download"] = "Completed"
-            progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
+                    # Set reading in the new report as completed
+                    st.session_state["sheet_status"]["Loading New Report"] = "Completed"
+                    progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
+
+                    # Remove column not needing comparison
+                    old['Meter Activity'].drop(columns = [x for x in old['Meter Activity'].columns if 'Days Since Upload as of' in x], inplace = True)
+                    new['Meter Activity'].drop(columns = [x for x in new['Meter Activity'].columns if 'Days Since Upload as of' in x], inplace = True)
+
+                    # Generate the what's changed report
+                    result = gen_report(unique_keys, sheets_to_compare, progress_placeholder, old, new)
+
+                    # Store result in session_state to prevent rerunning upon download
+                    st.session_state['changed_report'] = result
+
+                    # Offer the file for download
+                    if 'changed_report' in st.session_state:
+                        st.download_button(
+                            label = "Download What's Changed Report",
+                            data = st.session_state["changed_report"],
+                            file_name = f"{datetime.now().strftime('%Y-%m-%d')} Changes.xlsx",
+                            mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
+                        )
+
+                        # set generating export to completed
+                        st.session_state["sheet_status"]["Generating Download"] = "Completed"
+                        progress_placeholder.markdown(display_progress_list(st.session_state["sheet_status"]))
+
+        else:
+            st.error('Invalid credentials.')
+    except Exception as e:
+        st.write(traceback.print_exc())
+else:
+    st.write('Please upload the credentials file.')
